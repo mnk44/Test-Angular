2e8cf02efa1e644b6adc4b7a4573c389186bd6fd
mnk44 <mnkmm44@gmail.com>
2024-02-04 23:47:07 -0500

    Making a product list

--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataTablesModule } from 'angular-datatables';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../model/product';
+import { ProductService } from '../../service/product.service';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [ 
+    DataTablesModule,
+    CommonModule
+  ],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  dtOptions: DataTables.Settings = {};
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10,
+      processing: true
+    };
+
+    this.productService.getProducts().subscribe(
+      (data) => {
+        this.products = data;
+        console.log(this.products);
+      },
+      (error) => {
+        console.error('Error fetching products:', error);
+      }
+    );
+  }
 }
